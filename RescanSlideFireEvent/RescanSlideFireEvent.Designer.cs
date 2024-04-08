4f881f62479ee81f3bd40f7fd48d81e39e8f0928
one1erp <avigaile@one1.co.il>
2024-04-08 17:31:14 +0300

    Initial commit with additional files

---
+++ RescanSlideFireEvent/RescanSlideFireEvent.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace RescanSlideFireEvent
+{
+    partial class RescanSlideFireEvent
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listViewEntities = new System.Windows.Forms.ListView();
+            this.close_button = new System.Windows.Forms.Button();
+            this.Ok_button = new System.Windows.Forms.Button();
+            this.txtEditEntity = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.listViewEntities);
+            this.panel1.Controls.Add(this.close_button);
+            this.panel1.Controls.Add(this.Ok_button);
+            this.panel1.Controls.Add(this.txtEditEntity);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(13, 26);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(836, 459);
+            this.panel1.TabIndex = 0;
+            // 
+            // listViewEntities
+            // 
+            this.listViewEntities.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.listViewEntities.GridLines = true;
+            this.listViewEntities.Location = new System.Drawing.Point(64, 109);
+            this.listViewEntities.Name = "listViewEntities";
+            this.listViewEntities.Size = new System.Drawing.Size(731, 283);
+            this.listViewEntities.TabIndex = 6;
+            this.listViewEntities.UseCompatibleStateImageBehavior = false;
+            this.listViewEntities.View = System.Windows.Forms.View.List;
+            // 
+            // close_button
+            // 
+            this.close_button.Location = new System.Drawing.Point(488, 404);
+            this.close_button.Name = "close_button";
+            this.close_button.Size = new System.Drawing.Size(75, 23);
+            this.close_button.TabIndex = 5;
+            this.close_button.Text = "Close";
+            this.close_button.UseVisualStyleBackColor = true;
+            this.close_button.Click += new System.EventHandler(this.close_button_Click);
+            // 
+            // Ok_button
+            // 
+            this.Ok_button.Location = new System.Drawing.Point(305, 404);
+            this.Ok_button.Name = "Ok_button";
+            this.Ok_button.Size = new System.Drawing.Size(75, 23);
+            this.Ok_button.TabIndex = 4;
+            this.Ok_button.Text = "OK";
+            this.Ok_button.UseVisualStyleBackColor = true;
+            this.Ok_button.Click += new System.EventHandler(this.Ok_button_Click);
+            // 
+            // txtEditEntity
+            // 
+            this.txtEditEntity.Location = new System.Drawing.Point(503, 70);
+            this.txtEditEntity.Name = "txtEditEntity";
+            this.txtEditEntity.Size = new System.Drawing.Size(137, 20);
+            this.txtEditEntity.TabIndex = 3;
+            this.txtEditEntity.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.aliquotName_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(647, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = ":הזן מספר סלייד";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(355, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "סליידים לסריקה חוזרת";
+            // 
+            // RescanSlideFireEvent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Name = "RescanSlideFireEvent";
+            this.Size = new System.Drawing.Size(871, 503);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtEditEntity;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listViewEntities;
+        private System.Windows.Forms.Button close_button;
+        private System.Windows.Forms.Button Ok_button;
+    }
+}
---
+++ RescanSlideFireEvent/RescanSlideFireEvent.cs
@@ -0,0 +1,446 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LSSERVICEPROVIDERLib;
+using LSExtensionWindowLib;
+using Oracle.ManagedDataAccess.Client;
+using Patholab_DAL_V1;
+using Patholab_Common;
+using System.Runtime.InteropServices;
+
+namespace RescanSlideFireEvent
+{
+    [ComVisible(true)]
+    [ProgId("RescanSlideFireEvent.RescanSlideFireEvent")]
+    public partial class RescanSlideFireEvent : UserControl, IExtensionWindow
+    {
+        public RescanSlideFireEvent()
+        {
+            InitializeComponent();
+            aliquots = new List<ALIQUOT>();
+        }
+
+        #region Private Members
+
+        private INautilusUser _ntlsUser;
+        private IExtensionWindowSite2 _ntlsSite;
+
+        private INautilusProcessXML _processXml;
+        INautilusDBConnection _ntlsCon;
+
+
+        private INautilusServiceProvider _sp;
+
+        private OracleConnection _connection;
+
+        private OracleCommand cmd;
+
+        private INautilusRecordSet rs;
+
+
+        private List<ALIQUOT> aliquots;
+
+        private double sessionId;
+
+        private string _connectionString;
+
+        #endregion
+
+        #region Implementing IExtensionWindow
+        public bool CloseQuery()
+        {
+            try
+            {
+
+
+                if (cmd != null)
+                    cmd.Dispose();
+                if (_connection != null)
+                    _connection.Close();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                return true;
+            }
+        }
+
+        public WindowRefreshType DataChange()
+        {
+            return LSExtensionWindowLib.
+                WindowRefreshType.windowRefreshNone;
+        }
+
+        public WindowButtonsType GetButtons()
+        {
+            return LSExtensionWindowLib.WindowButtonsType.windowButtonsNone;
+        }
+
+        public void Internationalise()
+        {
+        }
+
+        public void PreDisplay()
+        {
+            INautilusDBConnection dbConnection;
+            if (_sp != null)
+            {
+
+                // Debugger.Launch();
+                dbConnection = _sp.QueryServiceProvider("DBConnection") as NautilusDBConnection;
+                rs = _sp.QueryServiceProvider("RecordSet") as NautilusRecordSet;
+
+
+
+            }
+            else
+            {
+                dbConnection = null;
+            }
+
+
+
+            _connection = GetConnection(dbConnection);
+        }
+
+        public void RestoreSettings(int hKey)
+        {
+        }
+
+        public bool SaveData()
+        {
+            return false;
+        }
+
+        public void SaveSettings(int hKey)
+        {
+        }
+
+        public void SetParameters(string parameters)
+        {
+        }
+
+        public void SetServiceProvider(object serviceProvider)
+        {
+            _sp = serviceProvider as NautilusServiceProvider;
+
+            if (_sp != null)
+            {
+                _processXml = _sp.QueryServiceProvider("ProcessXML") as NautilusProcessXML;
+                _ntlsUser = _sp.QueryServiceProvider("User") as NautilusUser;
+                _ntlsCon = Utils.GetNtlsCon(_sp);
+            }
+            else
+            {
+                _processXml = null;
+            }
+        }
+
+        public void SetSite(object site)
+        {
+            _ntlsSite = (IExtensionWindowSite2)site;
+            _ntlsSite.SetWindowInternalName("Fire Event");
+            _ntlsSite.SetWindowRegistryName("Fire Event");
+            _ntlsSite.SetWindowTitle("Fire Event");
+        }
+
+        public void Setup()
+        {
+        }
+
+        public WindowRefreshType ViewRefresh()
+        {
+            return LSExtensionWindowLib.WindowRefreshType.windowRefreshNone;
+        }
+
+        public void refresh()
+        {
+        }
+
+        #endregion
+
+
+        #region Private methods
+
+        public OracleConnection GetConnection(INautilusDBConnection ntlsCon)
+        {
+
+            OracleConnection connection = null;
+
+            if (ntlsCon != null)
+            {
+
+
+                // Initialize variables
+                String roleCommand;
+                // Try/Catch block
+                try
+                {
+
+
+                    var C = ntlsCon.GetServerIsProxy();
+                    var C2 = ntlsCon.GetServerName();
+                    var C4 = ntlsCon.GetServerType();
+
+                    var C6 = ntlsCon.GetServerExtra();
+
+                    var C8 = ntlsCon.GetPassword();
+                    var C9 = ntlsCon.GetLimsUserPwd();
+                    var C10 = ntlsCon.GetServerIsProxy();
+                    var DD = _ntlsSite;
+
+
+
+
+                    var u = _ntlsUser.GetOperatorName();
+                    var u1 = _ntlsUser.GetWorkstationName();
+
+
+
+                    _connectionString = ntlsCon.GetADOConnectionString();
+
+                    var splited = _connectionString.Split(';');
+
+                    var cs = "";
+
+                    for (int i = 1; i < splited.Count(); i++)
+                    {
+                        cs += splited[i] + ';';
+                    }
+                    //<<<<<<< .mine
+                    var username = ntlsCon.GetUsername();
+                    if (string.IsNullOrEmpty(username))
+                    {
+                        var serverDetails = ntlsCon.GetServerDetails();
+                        cs = "User Id=/;Data Source=" + serverDetails + ";";
+                    }
+
+
+                    //Create the connection
+                    connection = new OracleConnection(cs);
+
+
+
+                    // Open the connection
+                    connection.Open();
+
+                    // Get lims user password
+                    string limsUserPassword = ntlsCon.GetLimsUserPwd();
+
+                    // Set role lims user
+                    if (limsUserPassword == "")
+                    {
+                        // LIMS_USER is not password protected
+                        roleCommand = "set role lims_user";
+                    }
+                    else
+                    {
+                        // LIMS_USER is password protected.
+                        roleCommand = "set role lims_user identified by " + limsUserPassword;
+                    }
+
+                    // set the Oracle user for this connecition
+                    OracleCommand command = new OracleCommand(roleCommand, connection);
+
+                    // Try/Catch block
+                    try
+                    {
+                        // Execute the command
+                        command.ExecuteNonQuery();
+                    }
+                    catch (Exception f)
+                    {
+                        // Throw the exception
+                        throw new Exception("Inconsistent role Security : " + f.Message);
+                    }
+
+                    // Get the session id
+                    sessionId = ntlsCon.GetSessionId();
+
+                    // Connect to the same session
+                    string sSql = string.Format("call lims.lims_env.connect_same_session({0})", sessionId);
+
+                    // Build the command
+                    command = new OracleCommand(sSql, connection);
+
+                    // Execute the command
+                    command.ExecuteNonQuery();
+
+                }
+                catch (Exception e)
+                {
+                    // Throw the exception
+                    throw e;
+                }
+
+                // Return the connection
+            }
+
+            return connection;
+
+        }
+        #endregion
+
+        private void aliquotName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string sql = "";
+            try
+            {
+                if (e.KeyChar == (char)13 && txtEditEntity.Text != "") //Enter
+                {
+
+                    //Checks if it's already in list view
+                    if (!ListViewContains())
+                    {
+                        //Build query
+                        if (!string.IsNullOrEmpty(txtEditEntity.Text))
+                        {
+                            sql = "select erd.U_SLIDE_NAME from lims_sys.u_extra_request_data_user erd inner join lims_sys.u_extra_request er " +
+                                "on erd.U_EXTRA_REQUEST_ID = er.U_EXTRA_REQUEST_ID " +
+                                "where er.name like '%Rescan%' and u_status='V' and U_REQ_TYPE='S' and erd.U_SLIDE_NAME = '" + txtEditEntity.Text +"'";
+                        }
+                        
+                        cmd = new OracleCommand(sql, _connection);
+                        OracleDataReader reader = cmd.ExecuteReader();
+
+                        //Checks if it exists
+                        if (!reader.HasRows)
+                        {
+                            MessageBox.Show(txtEditEntity.Text +
+                                "  does not exist or does not meet the conditions!", "Nautilus",
+                                MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                            txtEditEntity.Focus();
+                        }
+                        else
+                        {
+                            ListViewItem li = null;
+                            while (reader.Read())
+                            {
+                                li = new ListViewItem(reader[0].ToString());
+                                string name = reader[0].ToString();
+                            }
+                            listViewEntities.Items.Add(li);
+                            txtEditEntity.Text = string.Empty;
+                            reader.Close();
+                            listViewEntities.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show(txtEditEntity.Text + "  already exists");
+                        txtEditEntity.Focus();
+                    }
+                }
+            }
+            catch (Exception e1)
+            {
+                MessageBox.Show("Error: " + e1.Message + e1.StackTrace);
+                //Logger.WriteLogFile(e1);
+            }
+        }
+
+        private bool ListViewContains()
+        {
+            foreach (ListViewItem item in listViewEntities.Items)
+            {
+                if (item.SubItems[0].Text == txtEditEntity.Text)
+                    return true;
+            }
+            return false;
+        }
+
+        private void Ok_button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (ListViewItem item in listViewEntities.Items)
+                {
+                    RunEvent(item.SubItems[0].Text);
+                    Add2Log(item.SubItems[0].Text);
+                }
+                //Empties the list
+                listViewEntities.Clear();
+                MessageBox.Show("Completed",this.Name,MessageBoxButtons.OK);
+            }
+            catch (Exception e1)
+            {
+                MessageBox.Show("Error" + e1.Message);
+
+                //Logger.WriteLogFile(e1);
+            }
+        }
+
+        //event needs to be implemented later
+        private void RunEvent(string name)
+        {
+            string sql = "select u_slide_name from lims_sys.U_EXTRA_REQUEST_DATA_USER where U_REQ_TYPE='S' and   u_status='V' and U_SLIDE_NAME = '" + name + "'";
+            cmd = new OracleCommand(sql, _connection);
+            OracleDataReader reader = cmd.ExecuteReader();
+
+            //Checks if it exists
+            if (!reader.HasRows)
+            {
+                MessageBox.Show(name +
+                    "  does not exist or does not meet the conditions!", "Nautilus",
+                    MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                txtEditEntity.Focus();
+            }
+            else
+            {
+                while (reader.Read())
+                {
+                    string ex = reader[0].ToString();
+                    sql = "update lims_sys.U_EXTRA_REQUEST_DATA_USER set u_status = 'L', U_LAB_ON = to_char(sysdate) where U_REQ_TYPE='S' and   u_status='V' and U_SLIDE_NAME = '" + ex + "'";
+                    cmd = new OracleCommand(sql, _connection);
+                    cmd.ExecuteReader();
+                }
+            }
+            reader.Close();
+
+        }
+
+        //log needs to be implemented later
+        private void Add2Log(string id)
+        {
+        }
+
+        private void close_button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listViewEntities.Items.Count > 0)
+                {
+                    DialogResult dialogResult = MessageBox.Show("האם אתה בטוח שברצונך לצאת ממסך זה ללא אישור? ", "יציאה",
+                        MessageBoxButtons.YesNoCancel);
+                    if (dialogResult == DialogResult.Yes)
+                    {
+                        listViewEntities = null;
+                        if (_ntlsSite != null)
+                            _ntlsSite.CloseWindow();
+                    }
+                }
+                else
+                {
+                    listViewEntities = null;
+                    if (_ntlsSite != null)
+                        _ntlsSite.CloseWindow();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+    }
+    
+}
